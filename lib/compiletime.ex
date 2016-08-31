d6595ca69cac61b03216f0a27fbb9139fe86b0d2
Michael Pope <mpope.cr@gmail.com>
2016-08-31 08:38:40 -0400

    Gut application to prepare for reimplementation

--- lib/compiletime.ex
+++ lib/compiletime.ex
@@ -1,5 +1,2 @@
 defmodule Compiletime do
-  def fetch_app_env!(app, key, options \\ []) do
-    Compiletime.AppEnvFetcher.fetch!(app, key, options)
-  end
 end
--- test/compiletime_test.exs
+++ test/compiletime_test.exs
@@ -1,8 +1,3 @@
 defmodule CompiletimeTest do
   use ExUnit.Case
-  doctest Compiletime
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
