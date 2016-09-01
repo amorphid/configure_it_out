47063f213a91b474f0fd3b613478503a36f0eccd
Michael Pope <mpope.cr@gmail.com>
2016-09-01 14:53:12 -0400

    Add typespecs

--- lib/configure_it_out.ex
+++ lib/configure_it_out.ex
@@ -1,8 +1,16 @@
 defmodule ConfigureItOut do
+  @type app_id      :: ConfigureItOut.EnvFetcher.app_id()
+  @type config_key  :: ConfigureItOut.EnvFetcher.config_key()
+  @type config_val  :: ConfigureItOut.EnvFetcher.config_val()
+  @type default_val :: ConfigureItOut.EnvFetcher.default_val()
+  @type options     :: ConfigureItOut.EnvFetcher.options()
+
+  @spec fetch_env!(app_id(), config_key(), default_val()) :: config_val()
   def fetch_env!(app_id, config_key, default_val) do
     fetch_env!(app_id, config_key, default_val, [])
   end
 
+  @spec fetch_env!(app_id(), config_key(), default_val(), options) :: config_val()
   def fetch_env!(app_id, config_key, default_val, options) do
     ConfigureItOut.EnvFetcher.new(app_id, config_key, default_val, options)
     |> ConfigureItOut.EnvFetcher.fetch!()
--- lib/configure_it_out/env_fetcher.ex
+++ lib/configure_it_out/env_fetcher.ex
@@ -1,14 +1,23 @@
 defmodule ConfigureItOut.EnvFetcher do
   require Logger
 
+  @type app_id      :: atom()
+  @type config_key  :: atom()
+  @type config_val  :: any()
+  @type default_val :: any()
+  @type options     :: Keyword.t
+  @type t           :: struct()
+
   @enforce_keys [:app_id, :config_key, :suggested_val]
   defstruct     @enforce_keys
 
+  @spec fetch!(t()) :: config_val()
   def fetch!(%__MODULE__{}=state) do
     state
     |> fetch_config_val()
   end
 
+  @spec new(app_id(), config_key(), default_val(), options()) :: t()
   def new(app_id, config_key, default_val, options) do
     %__MODULE__{
       app_id:        app_id,
@@ -17,20 +26,22 @@ defmodule ConfigureItOut.EnvFetcher do
     }
   end
 
+  @spec fetch_config_val(t()) :: config_val()
   defp fetch_config_val(%__MODULE__{app_id:     app_id,
                                     config_key: config_key}=state) do
     case Application.fetch_env(app_id, config_key) do
       {:ok, config_val} -> config_val
-      :error            -> raise fetch_config_val_error(state)
+      :error            -> raise_fetch_config_val_error(state)
     end
   end
 
-  defp fetch_config_val_error(%__MODULE__{
-                                app_id:        app_id,
-                                config_key:    config_key,
-                                suggested_val: suggested_val
-                              }=_state) do
-    """
+  @spec raise_fetch_config_val_error(t()) :: no_return()
+  defp raise_fetch_config_val_error(%__MODULE__{
+                                      app_id:        app_id,
+                                      config_key:    config_key,
+                                      suggested_val: suggested_val
+                                    }=_state) do
+    raise """
 
     #{IO.ANSI.red()}
     No application environment variable for '#{inspect(app_id)}, #{inspect(config_key)}'.  Consider adding this to your '#{Mix.env}' config:
